cdc002fd3fd7de8afb9928e33ab974971b1f183e
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2021-08-01 18:08:44 +0100

    try to push to heroku 2

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ if(process.env.NODE_ENV != "production" ) require("dotenv").config()
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app=express()
-const port= process.env.port || 5000
+const port = process.env.PORT || 5000;
 app.use(cors())
 
 app.use(bodyParser.json())
@@ -26,7 +26,6 @@ if(process.env.NODE_ENV=="production"){
 app.listen(port,error=>{
     if(error){
   throw error
-  console.log("yooooooooo error")
   ;   
     } 
     console.log("Server running in ",port)
